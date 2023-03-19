b92960f62460b1ccf972c2b82b72bd6b5aeaff00
Irina <116427830+11Rin@users.noreply.github.com>
2023-03-19 14:55:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,34 +1,34 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-	int left = 0;
-	int s = 0;
-	int s1 = 0;
-	int cnt = 1;
-	int right = size-1;
-	int mid = right / 2;
-	while (right >= left) {
-		if (value == arr[mid]) {
-			s = mid;
-			s1 = mid;
-			while (value == arr[s - 1]) {
-				cnt++;
-				s--;
-			}
-			while (value == arr[s1 + 1]) {
-				cnt++;
-				s1++;
-			}
-			return cnt;
-		}
-		if (value > arr[mid]) {
-			left = mid + 1;
-			mid = (right + left) / 2;
-		}
-		if (value < arr[mid]) {
-			right = mid - 1;
-			mid = (right + left) / 2;
-		}
-	}
-	return 0;
+  int left = 0;
+  int s = 0;
+  int s1 = 0;
+  int cnt = 1;
+  int right = size - 1;
+  int mid = right / 2;
+  while (right >= left) {
+    if (value == arr[mid]) {
+      s = mid;
+      s1 = mid;
+      while (value == arr[s - 1]) {
+        cnt++;
+	s--;
+      }
+      while (value == arr[s1 + 1]) {
+        cnt++;
+        s1++;
+      }
+      return cnt;
+    }
+    if (value > arr[mid]) {
+      left = mid + 1;
+      mid = (right + left) / 2;
+    }
+    if (value < arr[mid]) {
+      right = mid - 1;
+      mid = (right + left) / 2;
+    }
+  }
+  return 0;
 }
